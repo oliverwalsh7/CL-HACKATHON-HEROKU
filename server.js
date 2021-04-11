8da7298978454d5bb97fc2e0a079f6e1cb1dc885
oliverwalsh7 <oliverwalsh7@gmail.com>
2021-04-11 12:48:44 -0400

    LETS GO! express

--- server.js
+++ server.js
@@ -19,21 +19,24 @@ startExpress = async() => {
   var allowedOrigins = [
     'https://electchain-scvs.herokuapp.com/',
     `https://electchain-scvs.herokuapp.com/${process.env.PORT}`,
+    `https://electchain-scvs.herokuapp.com/${process.env.PWD}`,
     'http://localhost:8081'];
     
     app.use(cors({
       
-      origin: function(origin, callback){
-        // allow requests with no origin
-        // (like mobile apps or curl requests)
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-          var msg = 'The CORS policy for this site does not ' +
-          'allow access from the specified Origin.';
-          return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-      },
+      origin: "https://electchain-scvs.herokuapp.com/",
+
+      // origin: function(origin, callback){
+      //   // allow requests with no origin
+      //   // (like mobile apps or curl requests)
+      //   if(!origin) return callback(null, true);
+      //   if(allowedOrigins.indexOf(origin) === -1){
+      //     var msg = 'The CORS policy for this site does not ' +
+      //     'allow access from the specified Origin.';
+      //     return callback(new Error(msg), false);
+      //   }
+      //   return callback(null, true);
+      // },
       
       exposedHeaders: ['Access-Control-Allow-Headers', 'Access-Control-Allow-Origin'],
       
