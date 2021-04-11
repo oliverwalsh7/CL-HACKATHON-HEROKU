8127f53fb767d826b5aeb0cfe3493454424e6d40
oliverwalsh7 <oliverwalsh7@gmail.com>
2021-04-11 13:43:35 -0400

    LETS GO! express

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ startExpress = async() => {
       // allow requests with no origin
       // (like mobile apps or curl requests)
       if(!origin) return callback(null, true);
-      if(allowedOrigins.indexOf(origin) === -1){
+      if(allowedOrigins.indexOf(origin) === 1){
         var msg = 'The CORS policy for this site does not ' +
         'allow access from the specified Origin:' + origin;
         return callback(new Error(msg), false);
