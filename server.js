bd03543455a588e307e9f9eaf0cf06d5db0c4528
oliverwalsh7 <oliverwalsh7@gmail.com>
2021-04-11 11:43:39 -0400

    LETS GO! express

--- server.js
+++ server.js
@@ -16,31 +16,33 @@ var resp = ''
 startExpress = async() => {
   const app = express()
 
+  
   var allowedOrigins = [
     'https://electchain-scvs.herokuapp.com/',
     `https://electchain-scvs.herokuapp.com/${process.env.PORT}`,
     'http://localhost:8081'];
     
-  app.use(cors({
-
-  origin: function(origin, callback){
-    // allow requests with no origin
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-
-  exposedHeaders: ['Access-Control-Allow-Headers', 'Access-Control-Allow-Origin'],
-
-  credentials: true,
-}));
-
-app.use(express.static(path.join(__dirname, 'public')));
+    app.use(cors({
+      
+      origin: function(origin, callback){
+        // allow requests with no origin
+        // (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+          var msg = 'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.';
+          return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+      },
+      
+      exposedHeaders: ['Access-Control-Allow-Headers', 'Access-Control-Allow-Origin'],
+      
+      credentials: true,
+    }));
+    
+  process.env.PWD = process.cwd();
+  app.use(express.static(path.join(process.env.PWD, 'public')));
 
   app.get("/*", cors({
 
